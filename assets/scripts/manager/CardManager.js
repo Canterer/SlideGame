3e4f6755fc4a3f23439bc202453410c54f0625bf
Canterer <1170836331@qq.com>
2019-03-01 22:06:51 +0800

    1

--- assets/scripts/manager/CardManager.js
+++ assets/scripts/manager/CardManager.js
@@ -9,10 +9,10 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
-        row: 5,//卡牌行数
+        row: 8,//卡牌行数
         col: 5,//卡牌列数
-        gap: 5,//卡牌间距
-        squareFlag: true,
+        gap: 40,//卡牌间距
+        squareFlag: false,
         cardNodePrefab:{
             default: null,
             type:cc.Prefab
@@ -24,18 +24,20 @@ cc.Class({
         this.cardSizeX = (cc.winSize.width - this.gap*(this.col+1))/this.col;
         if(this.squareFlag)
             this.cardSizeY = this.cardSizeX;
-        else
-            this.cardSizeY = (cc.winSize.width - this.gap*(this.row+1))/this.row;
+        else{
+            var height = this.cardContent.height;
+            this.cardSizeY = (height - this.gap*(this.row+1))/this.row;
+        }
+
         this.cardPositons = [];
 
         // this.cardNodes = [];
-        // for (var i = 0; i < this.row; ++i) {
-        //     for (var j =  0; j < this.col; ++j) {
-        //         // var node = cc.instantiate(this.cardNodePrefab);
-        //         this.cardNodes[i*this.col+j] = self:createCard(i,j)
-        //     }
-        //     this.cardNodes[i]
-        // }
+        for (var i = 1; i <= this.row; ++i) {
+            for (var j =  1; j <= this.col; ++j) {
+                var x = j*(this.gap+this.cardSizeX) - this.cardSizeX/2;
+                this.createCard(i,j, cc.p(x, 2000));
+            }
+        }
     },
 
     start () {
@@ -44,8 +46,17 @@ cc.Class({
 
     createCard:function(row, col, position)
     {
-        var x = row*(this.gap+this.cardSizeX) - this.cardSizeX/2;
-        var y = col*(this.gap+this.cardSizeY) - this.cardSizeY/2;
+        var node = cc.instantiate(this.cardNodePrefab);
+        node.setParent(this.cardContent);
+        var x = col*(this.gap+this.cardSizeX) - this.cardSizeX/2;
+        var y = row*(this.gap+this.cardSizeY) - this.cardSizeY/2;
+        cc.log(x,y);
+        node.width = this.cardSizeX;
+        node.height = this.cardSizeY;
+        node.setPosition(position);
+
+        var action = cc.moveTo(1, cc.v2(x,y));
+        node.runAction(action);
 
         // var type = 3;
         // if(type in CardColors)
